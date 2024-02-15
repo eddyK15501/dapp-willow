1d0ec360039e0a941796503e5476d8f62c90efbd
Edward Kim <itgma1231@gmail.com>
2024-02-15 13:33:58 -0500

    ERC721 RealEstate.sol; safeMint() with modifier inherited from Ownable.sol - onlyOwner

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -1,6 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract RealEstate {
-    
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+import "hardhat/console.sol";
+
+contract RealEstate is ERC721, Ownable {
+    uint256 private _nextTokenId;
+    constructor(address initialOwner) 
+        ERC721("Real Estate", "REAL") 
+        Ownable(initialOwner) 
+    {}
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+    }
 }
