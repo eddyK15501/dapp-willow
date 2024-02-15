1281bd78e9abee9026b45f390dcccb0cf45da300
Edward Kim <itgma1231@gmail.com>
2024-02-15 17:38:59 -0500

    Escrow.sol initial setup

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -2,5 +2,28 @@
 pragma solidity ^0.8.24;
 
 interface IERC721 {
-    function transferFrom(address _from, address _to, uint256 _id) external;
+    function transferFrom(
+        address _from, 
+        address _to, 
+        uint256 _id
+    ) external;
+}
+
+contract Escrow {
+    address public nftAddress;
+    address payable public seller;
+    address public lender;
+    address public inspector;
+
+    constructor(
+        address _nftAddress, 
+        address payable _seller, 
+        address _lender, 
+        address _inspector
+    ) {
+        nftAddress = _nftAddress;
+        seller = _seller;
+        lender = _lender;
+        inspector = _inspector;
+    }
 }
