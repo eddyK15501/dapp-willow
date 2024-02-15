cdb5fa074bd4b2d370954fb34e6e613c6c0ad1de
Edward Kim <itgma1231@gmail.com>
2024-02-15 18:55:32 -0500

    ethers.js deployment test for RealEstate.sol

--- test/Escrow.js
+++ test/Escrow.js
@@ -6,6 +6,14 @@ const tokens = (n) => {
     return ethers.utils.parseUnits(n.toString(), 'ether');
 }
 
-describe('Escrow', () => {
-    
-})
+describe('Escrow', () => {});
+
+describe('Real Estate', () => {
+    it('Sample test. Deploy RealEstate.sol smart contract. Saves the addresses', async () => {
+        [owner] = await ethers.getSigners();
+        const realEstate = await ethers.getContractFactory('RealEstate');
+        const rsDeployed = await realEstate.deploy(owner);
+
+        console.log(rsDeployed.target);
+    })
+});
