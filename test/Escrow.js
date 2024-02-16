39a664cc97062ca28164412dbde23f2450f3b811
Edward Kim <itgma1231@gmail.com>
2024-02-16 12:42:10 -0500

    chai expect nftAddress to equal deployed RealEstate contract address

--- test/Escrow.js
+++ test/Escrow.js
@@ -3,23 +3,43 @@ const { ethers } = require('hardhat');
 
 // Convert into equivalent value in wei
 const tokens = (n) => {
-    return ethers.utils.parseUnits(n.toString(), 'ether');
-}
+  return ethers.utils.parseUnits(n.toString(), 'ether');
+};
 
-describe('Escrow', () => {});
+describe('Escrow', () => {
+  let buyer, seller, lender, inspector;
+  let realEstate;
+  let escrow;
 
-describe('Real Estate', () => {
-    it('Deploy RealEstate.sol. Saves the addresses', async () => {
-        const signers = await ethers.getSigners();
-        const buyer = signers[0];
-        const seller = signers[1];
-        
-        // Deploy Real Estate Contract
-        const rsFactory = await ethers.getContractFactory('RealEstate');
-        const realEstate = await rsFactory.deploy();
-        console.log(realEstate.target);
+  it('Deploys the contracts, saves the addresses', async () => {
+    [buyer, seller, lender, inspector] = await ethers.getSigners();
 
-        // Mint
-        // let transaction = await realEstate.mint("https://ipfs.io/ipfs/QmdGJNJwL9aN1E25iAc8L2aR33SFQ8e2qDrnc7V26WJm2d?filename=1.json") 
-    })
+    // Deploy Real Estate Contract
+    const rsFactory = await ethers.getContractFactory('RealEstate');
+    realEstate = await rsFactory.deploy();
+    console.log(realEstate.target);
+
+    // Mint
+    let transaction = await realEstate
+      .connect(seller)
+      .mint(
+        'https://ipfs.io/ipfs/QmdGJNJwL9aN1E25iAc8L2aR33SFQ8e2qDrnc7V26WJm2d?filename=1.json'
+      );
+    await transaction.wait();
+
+    // Deploy Escrow Contract
+    const escrowFactory = await ethers.getContractFactory('Escrow');
+    escrow = await escrowFactory.deploy(
+      realEstate.target,
+      seller.address,
+      lender.address,
+      inspector.address
+    );
+    console.log(escrow.target);
+
+    const result = await escrow.nftAddress();
+    console.log(result);
+
+    expect(result).to.be.equal(realEstate.target);
+  });
 });
