ae57c49f1e9589d2e931ebe9856259dcbc76dbda
Edward Kim <itgma1231@gmail.com>
2024-02-16 12:21:43 -0500

    remove Ownable.sol inheritance - RealEstate.sol

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -3,18 +3,16 @@ pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
 import "hardhat/console.sol";
 
-contract RealEstate is ERC721URIStorage, Ownable {
+contract RealEstate is ERC721URIStorage {
     uint256 private _tokenId;
     constructor(address initialOwner) 
         ERC721("Real Estate", "REAL") 
-        Ownable(initialOwner) 
     {}
 
-    function mint(string memory tokenURI) public onlyOwner returns(uint256) {
+    function mint(string memory tokenURI) public returns(uint256) {
         _tokenId++;
         _safeMint(msg.sender, _tokenId);
         _setTokenURI(_tokenId, tokenURI);
--- test/Escrow.js
+++ test/Escrow.js
@@ -10,14 +10,16 @@ describe('Escrow', () => {});
 
 describe('Real Estate', () => {
     it('Deploy RealEstate.sol. Saves the addresses', async () => {
+        const signers = await ethers.getSigners();
+        const buyer = signers[0];
+        const seller = signers[1];
+        
         // Deploy Real Estate Contract
-        [owner] = await ethers.getSigners();
         const rsFactory = await ethers.getContractFactory('RealEstate');
-        const realEstate = await rsFactory.deploy(owner);
+        const realEstate = await rsFactory.deploy();
         console.log(realEstate.target);
 
         // Mint
-        // https://ipfs.io/ipfs/QmdGJNJwL9aN1E25iAc8L2aR33SFQ8e2qDrnc7V26WJm2d?filename=1.json
-        // let transaction = await realEstate.mint("https://ipfs.io/ipfs/QmdGJNJwL9aN1E25iAc8L2aR33SFQ8e2qDrnc7V26WJm2d?filename=1.json") 
+        let transaction = await realEstate.mint("https://ipfs.io/ipfs/QmdGJNJwL9aN1E25iAc8L2aR33SFQ8e2qDrnc7V26WJm2d?filename=1.json") 
     })
 });
