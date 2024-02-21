53aea228b9adaa62462a3b3135e8a352854a1306
Edward Kim <itgma1231@gmail.com>
2024-02-21 12:37:18 -0500

    BrowserProvider(window.ethereum) - ethers.js v6

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,25 @@
 import { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
 
-import config from '../config.json';
 import RealEstate from '../abi/RealEstate.json';
 import Escrow from '../abi/Escrow.json';
 
+import config from '../config.json';
+
 function App() {
+  const loadData = async () => {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    console.log(provider);
+  }
+
+  useEffect(() => {
+    loadData()
+  }, []);
+
   return (
     <div>
       <div className='cards__section'>
-        <h3>Welcome to Willow</h3>
+        <h3>Welcome to Willow!</h3>
       </div>
     </div>
   )
