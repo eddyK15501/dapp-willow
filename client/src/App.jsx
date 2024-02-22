7f4b4ae8a67abc58e4e8410dff07c9e6f25ea518
Edward Kim <itgma1231@gmail.com>
2024-02-22 15:55:18 -0500

    App.jsx - temporary card hardcoded for displayed; styling fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,26 @@
 import { useState, useEffect } from 'react';
-import { ethers } from 'ethers';
+// import { ethers } from 'ethers';
 import Navigation from './components/Navigation';
 
-import RealEstate from '../abi/RealEstate.json';
-import Escrow from '../abi/Escrow.json';
-import config from '../config.json';
+// import RealEstate from '../abi/RealEstate.json';
+// import Escrow from '../abi/Escrow.json';
+// import config from '../config.json';
 import Search from './components/Search';
 
 function App() {
   const [account, setAccount] = useState(null);
 
   const loadBlockchainData = async () => {
-    const provider = new ethers.BrowserProvider(window.ethereum);
+    // const provider = new ethers.BrowserProvider(window.ethereum);
 
     window.ethereum.on('accountsChanged', async () => {
-      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const accounts = await window.ethereum.request({
+        method: 'eth_requestAccounts',
+      });
       // const accountOne = ethers.utils?.getAddress(accounts[0]);
       setAccount(accounts[0]);
-    })
-  }
+    });
+  };
 
   useEffect(() => {
     loadBlockchainData();
@@ -30,9 +32,29 @@ function App() {
       <Search />
       <div className='cards__section'>
         <h3>Trending Homes</h3>
+        <p style={{ color: '#808080', fontSize: '0.85rem' }}>
+          Properties currently being viewed at the moment
+        </p>
+        <div className='cards'>
+          <div className="card">
+            <div className="card__image">
+              <img src="" alt="Home" />
+            </div>
+            <div className="card__info">
+              <h4>200 ETH</h4>
+              <p style={{ fontWeight: '400', marginTop: '1px' }}>
+                <strong>1</strong> bds <span>|</span>
+                <strong>{" "}2</strong> ba <span>|</span>
+                <strong>{" "}3</strong> sqft <span>|</span>
+                {" "}House for sale
+              </p>
+              <p style={{ fontSize: '0.85rem' }}>987 Oak Drive, Sunnydale, Faketown</p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
