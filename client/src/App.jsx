617a055e56534d0f2114bb40f605d27f2e0bfa41
Edward Kim <itgma1231@gmail.com>
2024-02-21 21:44:23 -0500

    Navigation.jsx - connect to metamask account

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,21 +8,18 @@ import config from '../config.json';
 
 function App() {
   const [account, setAccount] = useState(null);
-  console.log(account);
 
   const loadData = async () => {
     const provider = new ethers.BrowserProvider(window.ethereum);
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-    setAccount(accounts[0]);
   }
 
   useEffect(() => {
     loadData();
-  }, [setAccount]);
+  }, []);
 
   return (
     <>
-      <Navigation />
+      <Navigation account={account} setAccount={setAccount} />
       <div className='cards__section'>
         <h3>Welcome to Willow!</h3>
       </div>
--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,28 +1,35 @@
-import React from 'react'
+import React from 'react';
 
-const Navigation = () => {
+const Navigation = ({ account, setAccount }) => {
+  const connectHandler = async () => {
+    const accounts = await window.ethereum.request({
+      method: 'eth_requestAccounts',
+    });
+    setAccount(accounts[0]);
+  };
+
   return (
     <nav>
-        <ul className="nav__links">
-            <li>
-                <a href="#">Buy</a>
-            </li>
-            <li>
-                <a href="#">Rent</a>
-            </li>
-            <li>
-                <a href="#">Sell</a>
-            </li>
-        </ul>
-        <div className='nav__brand'>
-            <img src="" alt="" />
-            <h1>Willow</h1>
-        </div>
-        <button type='button' className='nav__connect'>
-            0x0...
-        </button>
+      <ul className='nav__links'>
+        <li>
+          <a href='#'>Buy</a>
+        </li>
+        <li>
+          <a href='#'>Rent</a>
+        </li>
+        <li>
+          <a href='#'>Sell</a>
+        </li>
+      </ul>
+      <div className='nav__brand'>
+        <img src='' alt='' />
+        <h1>Willow</h1>
+      </div>
+      <button type='button' className='nav__connect'>
+        0x0...
+      </button>
     </nav>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
