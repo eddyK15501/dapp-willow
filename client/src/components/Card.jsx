37d4cd2440a95070bff4f11a5f0b7457a5ad29a3
Edward Kim <itgma1231@gmail.com>
2024-02-24 15:47:50 -0500

    Card.jsx - render ipfs metadata in Card component; start onClick()

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -2,6 +2,10 @@
 import React from 'react';
 
 const Card = ({ homes }) => {
+  const toggleModal = (prop) => {
+    console.log(prop);
+  };
+
   return (
     <div className='cards__section'>
       <h3>Trending Homes</h3>
@@ -11,20 +15,23 @@ const Card = ({ homes }) => {
       <div className='cards'>
         {homes.map((home) => {
           return (
-            <div className='card' key={home.id}>
+            <div
+              className='card'
+              key={home.id}
+              onClick={() => toggleModal(home)}
+            >
               <div className='card__image'>
-                <img src='' alt='Home' />
+                <img src={home.image} alt='Home' />
               </div>
               <div className='card__info'>
-                <h4>200 ETH</h4>
+                <h4>{home.attributes[0].value} ETH</h4>
                 <p style={{ fontWeight: '400', marginTop: '1px' }}>
-                  <strong>1</strong> bds <span>|</span>
-                  <strong> 2</strong> ba <span>|</span>
-                  <strong> 3</strong> sqft <span>|</span> House for sale
+                  <strong>{home.attributes[2].value}</strong> bds <span>|</span>
+                  <strong> {home.attributes[3].value}</strong> ba <span>|</span>
+                  <strong> {home.attributes[4].value}</strong> sqft{' '}
+                  <span>|</span> House for sale
                 </p>
-                <p style={{ fontSize: '0.85rem' }}>
-                  987 Oak Drive, Sunnydale, Faketown
-                </p>
+                <p style={{ fontSize: '0.85rem' }}>{home.address}</p>
               </div>
             </div>
           );
