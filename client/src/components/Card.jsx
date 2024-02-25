67efc7f54ddb0437bbfe95a41bab7f630d4a75b3
Edward Kim <itgma1231@gmail.com>
2024-02-24 19:36:24 -0500

    Home.jsx created; Toggle from Card.jsx

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,43 +1,53 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import { useState } from 'react';
+import Home from './Home';
 
 const Card = ({ homes }) => {
-  const toggleModal = (prop) => {
-    console.log(prop);
+  const [home, setHome] = useState({});
+  const [toggle, setToggle] = useState(false);
+
+  const toggleHome = (prop) => {
+    setHome(prop);
+    !toggle ? setToggle(true) : setToggle(false);
   };
 
   return (
-    <div className='cards__section'>
-      <h3>Trending Homes</h3>
-      <p style={{ color: '#808080', fontSize: '0.85rem' }}>
-        Properties currently being viewed at the moment
-      </p>
-      <div className='cards'>
-        {homes.map((home) => {
-          return (
-            <div
-              className='card'
-              key={home.id}
-              onClick={() => toggleModal(home)}
-            >
-              <div className='card__image'>
-                <img src={home.image} alt='Home' />
+    <>
+      <div className='cards__section'>
+        <h3>Trending Homes</h3>
+        <p style={{ color: '#808080', fontSize: '0.85rem' }}>
+          Properties currently being viewed at the moment
+        </p>
+        <div className='cards'>
+          {homes.map((home) => {
+            return (
+              <div
+                className='card'
+                key={home.id}
+                onClick={() => toggleHome(home)}
+              >
+                <div className='card__image'>
+                  <img src={home.image} alt='Home' />
+                </div>
+                <div className='card__info'>
+                  <h4>{home.attributes[0].value} ETH</h4>
+                  <p style={{ fontWeight: '400', marginTop: '1px' }}>
+                    <strong>{home.attributes[2].value}</strong> bds{' '}
+                    <span>|</span>
+                    <strong> {home.attributes[3].value}</strong> ba{' '}
+                    <span>|</span>
+                    <strong> {home.attributes[4].value}</strong> sqft{' '}
+                    <span>|</span> House for sale
+                  </p>
+                  <p style={{ fontSize: '0.85rem' }}>{home.address}</p>
+                </div>
               </div>
-              <div className='card__info'>
-                <h4>{home.attributes[0].value} ETH</h4>
-                <p style={{ fontWeight: '400', marginTop: '1px' }}>
-                  <strong>{home.attributes[2].value}</strong> bds <span>|</span>
-                  <strong> {home.attributes[3].value}</strong> ba <span>|</span>
-                  <strong> {home.attributes[4].value}</strong> sqft{' '}
-                  <span>|</span> House for sale
-                </p>
-                <p style={{ fontSize: '0.85rem' }}>{home.address}</p>
-              </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
-    </div>
+      {toggle && <Home />}
+    </>
   );
 };
 
